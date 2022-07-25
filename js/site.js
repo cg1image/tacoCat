0cf37cb45e4a5ba23ab5e4a60be1a6802e4ce642
CGovers <54491781+cg1image@users.noreply.github.com>
2022-07-24 21:17:01 -0700

    Update
    Creating Javascript for Palindrome project. Not complete

--- js/site.js
+++ js/site.js
@@ -1,26 +1,25 @@
+//get our user input
+function getValue(){
 
+    //get user string for the page
+    let unserString = document.getElementById("userString").value
 
+    //check for a palindrome
+    let returnObj = checkForPalindrome(userString);
 
-//get the values from the Page
-//starts or controller function
-function getValues(){
-    //get values from the page
-    let startValue = document.getElementById("startValue")
-
-
-    //we need to call generateNumbers
-
-    //we call displayNumbers
+    //display out message to the screen
+    displayMessage(returnObj);
 }
 
-//generate numbers from startValue to the endValue
-//logic function(s)
-function gnerateNumbers(){
+//check if string is a palindrome
+function checkForPalindrome(userString){
+
+
 
 }
 
-//dispay the numbers and mark even numbers bold
-//display or view functions
-function displayNumbers(){
+//display a message to the string.
+function displayMessage(){
+
 
 }
