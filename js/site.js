b7e7c437bb3480a992549aea1c0866a608599490
CGovers <54491781+cg1image@users.noreply.github.com>
2022-07-25 21:03:36 -0700

    Update Taco Cat
    D/L template and carved new Taco Cat javascript project. Not Complete

--- js/site.js
+++ js/site.js
@@ -1,25 +1,63 @@
+//html id--userString,btnSubmit,msg
+
+//items to look up- regex, reversing a string, 
+
 //get our user input
+//controller function
 function getValue(){
 
-    //get user string for the page
-    let unserString = document.getElementById("userString").value
+    //make sure the alert is invisible
+    document.getElementById("alert").classList.add("invisible");
+
+    //get user string from the page
+    let userString = document.getElementById("userString").value
 
     //check for a palindrome
     let returnObj = checkForPalindrome(userString);
-
+    
     //display out message to the screen
     displayMessage(returnObj);
 }
 
+
 //check if string is a palindrome
+//logic function
 function checkForPalindrome(userString){
+
+    userString = userString.toLowerCase();
+
+    //remove spaces and special characters
+    let regex = /[^a-z0-9]/gi;
+    userString = userString.replace(regex,"");
+
+    let revString = []; //array
+    let returnObj = {}; //object
+
+    for(let index = userString.length -1; index >=0; index--){
+        revString += userString[index];
+
+    }
+    //does userString = revString?
+    if (revString == userString) {
+        returnObj.msg = "Well done! You entered a palindrome!"
+    } else {
+       returnObj.msg = "Sorry! You did not enter a palindrome!" 
+    } 
+
+    returnObj.reversed = revString;
+
+    return returnObj;
 
 
 
 }
 
 //display a message to the string.
-function displayMessage(){
+//display function
+function displayMessage(returnObj){
 
+    document.getElementById("alertHeader").innerHTML.msg;
+    document.getElementById("msg").innerHTML = `Your phrase reversed is: ${returnObj.reversed}`;
+    document.getElementById("alert").classList.remove("invisible");
 
 }
